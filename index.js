41f53803fe982ca863af23b96c414df03a5ed985
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2022-02-23 16:10:24 +0100

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require("express-async-errors");
+const helmet = require("helmet");
+const compression = require("compression");
 const express = require("express");
 const app = express();
+
+app.use(helmet());
+app.use(compression());
 
 //startuj bazu
 require("./src/db")();

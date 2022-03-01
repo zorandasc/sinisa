0e41a287e13d44f52391d2d1c9d87e18537e968f
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2022-03-01 16:42:02 +0100

    added frontend react

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ require("./startup/db")();
 //definisi rute
 require("./startup/routes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  //for productiuion assets (bundle.js)
+  //requested from index.html, odnosno sve sto je
+  // potrebno react.js spa
+  app.use(express.static("client/build"));
+
+  //for home page index.html
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));

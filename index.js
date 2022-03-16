2be6541605320f0384e3bc0cee74672a8adb7970
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2022-03-16 15:06:07 +0100

    Pojednostavljen malo node index.js

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 require("express-async-errors");
 const helmet = require("helmet");
+const mongoose = require("mongoose");
 const compression = require("compression");
 const express = require("express");
 const app = express();
 
-app.use(helmet());
-app.use(compression());
+const keys = require("./config/keys");
+const users = require("./routes/users");
+const error = require("./middleware/error");
+
 
 //startuj bazu
-require("./startup/db")();
+//require("./startup/db")();
+mongoose
+  .connect(keys.mongoURI, {
+    //useFindAndModify: false,
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conected to mongodb");
+  })
+  .catch((err) => {
+    console.log("Eroro conecting to mongodb", err);
+  });
 
 //definisi rute
-require("./startup/routes")(app);
+//require("./startup/routes")(app);
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use("/api/users", users);//nasa api
+app.use(error); //middlewarre for catching promise rejections
 
 if (process.env.NODE_ENV === "production") {
   //for productiuion assets (bundle.js)
